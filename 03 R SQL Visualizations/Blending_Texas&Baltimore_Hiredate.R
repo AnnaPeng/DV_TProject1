780f1d6fb83656ab1e678ff374d2800cabecb35b
AnnaPeng <panyuanna@gmail.com>
2015-11-12 18:28:14 -0600

    adding blending

--- 03 R SQL Visualizations/Blending_Texas&Baltimore_Hiredate.R
+++ 03 R SQL Visualizations/Blending_Texas&Baltimore_Hiredate.R
@@ -1,10 +1,12 @@
 require("jsonlite")
 require("RCurl")
-require(dplyr)
+require("dplyr")
 
 BALTIMORE_SALARY <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from BALTIMORE_SALARY"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pp9774', PASS='orcl_pp9774', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 TEXAS_SALARY <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from TEXAS_SALARIES"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pp9774', PASS='orcl_pp9774', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-dplyr::inner_join(BALTIMORE_SALARY, TEXAS_SALARY, by="") %>% View
+df <- data.frame(dplyr::left_join(BALTIMORE_SALARY, TEXAS_SALARY, by="SERIALID"))
+#summary(df)
+head(df)
 
