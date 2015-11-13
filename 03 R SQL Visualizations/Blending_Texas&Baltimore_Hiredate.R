943a56c6b42e5f1b28b9ce3f7ce48e432f618399
AnnaPeng <panyuanna@gmail.com>
2015-11-12 18:27:57 -0600

    Adding blending

--- 03 R SQL Visualizations/Blending_Texas&Baltimore_Hiredate.R
+++ 03 R SQL Visualizations/Blending_Texas&Baltimore_Hiredate.R
@@ -6,5 +6,5 @@ BALTIMORE_SALARY <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:
 
 TEXAS_SALARY <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from TEXAS_SALARIES"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pp9774', PASS='orcl_pp9774', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-dplyr::inner_join(BALTIMORE_SALARY, TEXAS_SALARY, by="DEPTNO") %>% View
+dplyr::inner_join(BALTIMORE_SALARY, TEXAS_SALARY, by="") %>% View
 
